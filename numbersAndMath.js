a24a6a2b2bdd7ba5fb817e1306880e3e198df14b
gitxyogi <personalyogesh352@gmail.com>
2022-09-01 18:14:05 +0530

    first commit

---
+++ numbersAndMath.js
@@ -0,0 +1,16 @@
+result = 7.634635;
+result = result.toFixed(2);
+
+result = Math.PI;
+result = Math.E; 
+result = Math.round(6.7);
+result = Math.ceil(.7);
+result = Math.floor(9.9);
+result = Math.sqrt(64);
+result = Math.pow(6, 6);
+result = Math.abs(7878);
+result = Math.min(4, 6, 8, 2, -8, 34 - 43, 23, 0);
+result = Math.max(78, 88, 32, 4, 3, 78 - 34, 66);
+result = Math.random();
+
+console.log(result);
